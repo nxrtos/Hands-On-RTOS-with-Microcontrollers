f83013398aac9be860fe2285ace7d28a367119b9
Blue Now <bluenow1896@gmail.com>
2022-05-09 12:40:22 -0400

    drop in project Chapters05_06_stm32f0_disco_freeRTOS

---
+++ Chapters5_6/stm32f0_disco/stm32cubeide/Core/Inc/bsp.h
@@ -0,0 +1,33 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : bsp.h
+  * @brief          : Header for BSP package
+  *
+  ******************************************************************************
+  * @attention
+  *
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __BSP_H
+#define __BSP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <stm32f0_discovery.h>
+
+// extra make up
+#define LED_RED LED_GREEN
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __BSP_H */
